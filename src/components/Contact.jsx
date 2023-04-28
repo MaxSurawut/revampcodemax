daf244a9cf66998c9b159a68415085527386f00e
MaxSurawut <surawut.bao@gmail.com>
2023-04-28 21:48:06 +0700

    create api for contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
 import "./contact.scss";
+import axios from "axios";
+
 
 const Contact = ({ action, onClose }) => {
   const [isActive, setIsActive] = useState(false);
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [image, setImage] = useState(null)
 
   useEffect(() => {
     setIsActive(action);
@@ -12,7 +18,42 @@ const Contact = ({ action, onClose }) => {
     setIsActive(false);
     onClose();
   };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
+    axios.post('http://localhost:3000/customers', {
+      email: email,
+      name: name,
+      phone: phone,
+      image: image
+    })
+    .then(response => {
+      console.log(response)
+      alert('Data Saved')
+      setEmail('')
+      setName('')
+      setPhone('')
+      setImage(null)
+    })
+    .catch((error) => {
+      console.log(error)
+      alert('something wrong')
+    })
+
+
+  }
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+
+    reader.readAsBinaryString(file);
+  };
   return (
     <>
       <div
@@ -24,11 +65,19 @@ const Contact = ({ action, onClose }) => {
             <h2>ขอบคุณที่ให้ความสนใจในบริการของเรา</h2>
             <h2>กรุณากรอกข้อมูลและแนบตัวอย่างงานเพื่อประเมินราคา</h2>
           </div>
-          <form>
-            <input type="email" placeholder="อีเมลของคุณ" />
-            <input type="text" placeholder="ชื่อของคุณ" />
-            <input type="number" placeholder="เบอร์ติดต่อของคุณ" />
-            <input type="file" />
+          <form onSubmit={handleSubmit}>
+            <input type="email"
+              placeholder="อีเมลของคุณ"
+              onChange={(e) => { setEmail(e.target.value) }} />
+            <input type="text"
+              placeholder="ชื่อของคุณ"
+              onChange={(e) => { setName(e.target.value) }} />
+            <input type="number"
+              placeholder="เบอร์ติดต่อของคุณ"
+              onChange={(e) => { setPhone(e.target.value) }} />
+            <input type="file" 
+            accept="image/*"
+            onChange={handleFileChange} />
             <button>Submit</button>
           </form>
           <h5 onClick={handleClickActive}>ปิดหน้าต่างนี้</h5>
