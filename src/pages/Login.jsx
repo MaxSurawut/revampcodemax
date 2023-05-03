902456b77c16453d16421694402cc3f41a048607
MaxSurawut <surawut.bao@gmail.com>
2023-05-03 17:13:53 +0700

    commit loginpage layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,22 @@
-import './login.scss'
-
+import "./login.scss";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div className='login-container'>
-        <div className="login-box"></div>
+    <div className="login-container">
+      <div className="login-box">
+        <h2>CODEMAX</h2>
+        <form>
+          <input type="email" placeholder="your Email" />
+          <input type="password" placeholder="your Password"/>
+          <div className="btn-section">
+            <div className="homeBtn">Login</div>
+            <Link className="homeBtn" to='/'><div >Home</div></Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
